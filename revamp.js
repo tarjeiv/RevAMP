81d5de12d6eec2e4a8b0eea68b8f73668b3147f0
Tarjei Vassbotn <tarjei@google.com>
2012-01-06 16:55:43 +1100

    Initial version

---
+++ revamp.js
@@ -0,0 +1,28 @@
+ $(document).ready(function(){
+    function toggleTabs() {
+      $('#categories').toggle('slow');
+      $('#categoryContentTab').toggle('slow');
+      $('#createAccountTab').toggle('slow');
+    }
+    $('#cancelCreateAccountButton').click(toggleTabs);
+    $('#createAccountButton').click(function (e){
+      if (1) {toggleTabs()};
+    }); 
+
+    $('.showDetails').each(function(i,elm){
+      $(elm).click(function(e){
+        $(elm).parent().next().toggle('slow');
+        $(elm).toggleClass('showDetails');
+        $(elm).toggleClass('showingDetails');
+      })
+    });
+
+    //$('#createAccountButton').click();
+
+    $('.prefillable').keyup(function(e){
+      $('.prefilled').each(function(i,elm){
+        console.log(elm);
+      });
+    });
+
+ });
